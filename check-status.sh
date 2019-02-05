3f85c184619edfb28802426f2f611f50a4f1637e
Sam Willcocks <sam@wlcx.cc>
2019-02-05 12:16:51 +0000

    [meta] Make checker script check for upstream/head mismatch

--- check-status.sh
+++ check-status.sh
@@ -2,7 +2,14 @@
 
 # This script prints a noisy warning if the dotfiles repo has uncommited changes.
 # Best used at login.
+DOTFILE_DIR=$HOME/.dotfiles
 
-if ! git -C $HOME/.dotfiles diff --quiet --ignore-submodules HEAD ; then
-    echo "`tput setaf 1`OI! Commit your dotfiles!`tput sgr0`"
+if ! git -C $DOTFILE_DIR diff --quiet --ignore-submodules HEAD ; then
+    echo "`tput setaf 1`⚠️  Commit your dotfiles!`tput sgr0`"
+fi
+
+upstream=$(git -C $DOTFILE_DIR rev-parse @{u})
+head=$(git -C $DOTFILE_DIR rev-parse HEAD)
+if [ "$head" != "$upstream" ] ; then
+    echo "`tput setaf 1`⚠️  Push your dotfiles!`tput sgr0`"
 fi
